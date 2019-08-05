a6fd1ab90e8cecd0372b010620dedfbccb360f0e
lyx04 <https://github.com/lyx04>
2019-08-05 17:57:18 +0800

    表单验证

--- README.md
+++ README.md
@@ -6,9 +6,54 @@ maven { url 'https://maven.aliyun.com/repository/google' }
 maven { url 'https://maven.aliyun.com/repository/jcenter' }  
 maven { url 'http://maven.aliyun.com/nexus/content/groups/public'}  
 ## Form
-TextEditingController设置输入框的controller
->>输入框可以通过单类中的onchang(v){print(v)}/initstate中的controll.addListener((){print(controll.text)})
->>从第三个元素开始选中后面的文字
->>>_controll.selection = TextSelection(baseOfft:2//偏移两个字符,ectenetOffset:controll.text.length)
-
-
+TextEditingController是设置输入框的controller
+>输入框可以通过单类中的onchang(v){print(v)}/initstate中的controller.addListener((){print(controll.text)})
+从第三个元素开始选中后面的文字
+>controller.selection = TextSelection(baseOfft:2//偏移两个字符,ectenetOffset:controller.text.length)
+使用FormField实现单选框/校验
+>FormField(
+    onSaved: (value){
+        // userinfor.gender(value);
+    },
+    validator:(value){
+        if(value!=null){
+            Radionflag = true; 
+        return null;
+        }else{
+        setState(() {
+            Radionflag = false; 
+        });
+        }
+    },
+    builder: (field){
+        return Column(
+        children: <Widget>[
+            Radio(
+            groupValue:field.value,
+            value:1,
+            onChanged: (newvalue){
+                field.didChange(newvalue);
+            },
+            ),
+            Radio(
+            groupValue:field.value,
+            value:2,
+            onChanged: (newvalue){
+                field.didChange(newvalue);
+            },
+            ),
+            Radio(
+            groupValue:field.value,
+            value: 3,
+            onChanged: (newvalue){
+                field.didChange(newvalue);
+            },
+            ),
+            Offstage(
+            child: Text("单选为必选项"),
+            offstage:Radionflag ,
+            )
+        ],
+        );
+    },
+),
--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,417 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("字体样式"),
+            ),
+            body: BaseBox()));
+  }
+}
+
+class BaseBox extends StatefulWidget {
+  @override
+  _BaseBoxState createState() => _BaseBoxState();
+}
+
+class _BaseBoxState extends State<BaseBox> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        FontBox(),
+        ButtonBox(),
+        ImageBox(),
+        Checkedbox(),
+        TextBox(),
+        FormBox()
+      ],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class BoxTap extends StatefulWidget {
+  BoxTap({Key key, @required this.active}) : super(key: key);
+  bool active;
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _BoxTapState createState() => _BoxTapState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _BoxTapState extends State<BoxTap> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          widget.active = !widget.active;
+        });
+      },
+      child: Container(
+        width: 80.0,
+        height: 80.0,
+        decoration:
+            BoxDecoration(color: widget.active ? Colors.green : Colors.black),
+      ),
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+class FontBox extends StatefulWidget {
+  @override
+  _FontBoxState createState() => _FontBoxState();
+}
+
+class _FontBoxState extends State<FontBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("hello world", textAlign: TextAlign.left),
+        Text(
+          "heool worldfsdf sad asd ads" * 4,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+        Text(
+          "jdfkdjfk",
+          textScaleFactor: 1.5,
+        ),
+        DefaultTextStyle(
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 20.0,
+            ),
+            child: Column(
+              children: <Widget>[
+                Text("这里是继承的"),
+                Text("这里是不继承的",
+                    style: TextStyle(inherit: false, color: Colors.blue))
+              ],
+            )),
+        Text.rich(TextSpan(children: [
+          TextSpan(text: "home"),
+          TextSpan(
+            text: "www.baidu.com",
+            style: TextStyle(color: Colors.blue),
+            // recognizer: _tapRecognizer
+          )
+        ])),
+        Text(
+          "这是我的",
+          style: TextStyle(
+              color: Colors.blue,
+              fontSize: 18.0,
+              height: 1.2,
+              fontFamily: "Courier",
+              background: new Paint()..color = Colors.yellow,
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.dashed),
+        ),
+      ],
+    );
+  }
+}
+
+class ButtonBox extends StatefulWidget {
+  @override
+  _ButtonBoxState createState() => _ButtonBoxState();
+}
+
+class _ButtonBoxState extends State<ButtonBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        RaisedButton(
+          child: Text("自带阴影和灰色背景"),
+          onPressed: () {},
+        ),
+        FlatButton(
+          child: Text("扁平按钮"),
+          onPressed: () {},
+        ),
+        OutlineButton(
+          child: Text("有边框的按钮"),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () {},
+        ),
+        RaisedButton(
+            elevation: 2.0, //正常状态下阴影
+            highlightElevation: 9.0, //按下状态下阴影
+            disabledElevation: 10.0, //禁用状态下阴影
+            child: Text("自定义按钮"),
+            onPressed: () {},
+            color: Colors.blue,
+            textColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)))
+      ],
+    );
+  }
+}
+
+class ImageBox extends StatefulWidget {
+  @override
+  _ImageBoxState createState() => _ImageBoxState();
+}
+
+class _ImageBoxState extends State<ImageBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Image.network(
+          "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
+          width: 100.0,
+          color: Colors.red,
+          colorBlendMode: BlendMode.colorBurn,
+          // fit:BoxFit.fill,
+          // repeat:ImageRepeat.repeatX
+        ),
+        Icon(Icons.accessibility)
+      ],
+    );
+  }
+}
+
+class Checkedbox extends StatefulWidget {
+  @override
+  _CheckedboxState createState() => _CheckedboxState();
+}
+
+class _CheckedboxState extends State<Checkedbox> {
+  bool _switchSelected = true;
+  bool _checkboxSelected = null;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Switch(
+          value: _switchSelected,
+          onChanged: (bool flag) {
+            setState(() {
+              _switchSelected = flag;
+            });
+          },
+        ),
+        Checkbox(
+          tristate: true,
+          value: _checkboxSelected,
+          activeColor: Colors.red,
+          onChanged: (bool flag) {
+            setState(() {
+              _checkboxSelected = flag;
+            });
+          },
+        )
+      ],
+    );
+  }
+}
+
+class TextBox extends StatefulWidget {
+  @override
+  _TextBoxState createState() => _TextBoxState();
+}
+
+class _TextBoxState extends State<TextBox> {
+  TextEditingController _username = new TextEditingController();
+  FocusNode _usernamenode = new FocusNode();
+  FocusNode _passwordnode = new FocusNode();
+  @override
+  void initState() {
+    _username.text = "hell word";
+    // _username.selection = TextSelection(
+    //   baseOffset: 2,
+    //   extentOffset: _username.text.length
+    // );//选择两个字符以后的所有字符
+
+    // _username.addListener((){
+    //   print(_username);
+    // });
+    _usernamenode.addListener(() {
+      print(_usernamenode.hasFocus);
     });
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return Column(
+      children: <Widget>[
+        TextField(
+          // autofocus: true,
+          controller: _username,
+          focusNode: _usernamenode,
+          decoration: InputDecoration(
+              labelText: "用户名",
+              hintText: "用户名或邮箱",
+              prefixIcon: Icon(Icons.person)),
+          onChanged: (v) {
+            print(v);
+          },
+          // import "package:flutter/services.dart";用于textInputForamter
+          // inputFormatters:<TextInputFormatter>[
+
+          // ]
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        TextField(
+          focusNode: _passwordnode,
+          decoration: InputDecoration(
+              labelText: "密码",
+              hintText: "您的登录密码",
+              prefixIcon: Icon(Icons.lock)),
+          obscureText: true, //是否隐藏文本
+        ),
+        RaisedButton(
+          child: Text("提交"),
+          onPressed: () {
+            // print(_username);
+          },
+        ),
+        Builder(builder: (ctx) {
+          return Column(
+            children: <Widget>[
+              RaisedButton(
+                child: Text("将光标放置密码处"),
+                onPressed: () {
+                  _usernamenode.unfocus();
+                  FocusScope.of(context).requestFocus(_passwordnode);
+                },
+              ),
+              RaisedButton(
+                child: Text("隐藏键盘"),
+                onPressed: () {
+                  _usernamenode.unfocus();
+                  _passwordnode.unfocus();
+                },
+              )
+            ],
+          );
+        })
+      ],
     );
+  }
+}
+
+class FormBox extends StatefulWidget {
+  @override
+  _FormBoxState createState() => _FormBoxState();
+}
+
+class _FormBoxState extends State<FormBox> {
+  TextEditingController _username = TextEditingController();
+  TextEditingController _password = TextEditingController();
+  GlobalKey _formkey = new GlobalKey<FormState>();
+  bool Radionflag = true;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        // autovalidate:true,
+        key: _formkey,
+        child: Column(
+          children: <Widget>[
+            FormField(
+              onSaved: (value) {
+                // userinfor.gender(value);
+              },
+              validator: (value) {
+                if (value != null) {
+                  Radionflag = true;
+                  return null;
+                } else {
+                  setState(() {
+                    Radionflag = false;
+                  });
+                }
+              },
+              builder: (field) {
+                return Column(
+                  children: <Widget>[
+                    Radio(
+                      groupValue: field.value,
+                      value: 1,
+                      onChanged: (newvalue) {
+                        field.didChange(newvalue);
+                      },
+                    ),
+                    Radio(
+                      groupValue: field.value,
+                      value: 2,
+                      onChanged: (newvalue) {
+                        field.didChange(newvalue);
+                      },
+                    ),
+                    Radio(
+                      groupValue: field.value,
+                      value: 3,
+                      onChanged: (newvalue) {
+                        field.didChange(newvalue);
+                      },
+                    ),
+                    Offstage(
+                      child: Text("单选为必选项"),
+                      offstage: Radionflag,
+                    )
+                  ],
+                );
+              },
+            ),
+            TextFormField(
+              controller: _username,
+              decoration: InputDecoration(
+                  labelText: "用户名",
+                  hintText: "请输入用户名",
+                  prefixIcon: Icon(Icons.lock)),
+              onSaved: (value) {},
+              validator: (value) {
+                return value.trim().length > 0 ? null : "用户名不能为空";
+              },
+            ),
+            TextFormField(
+              controller: _password,
+              decoration: InputDecoration(
+                  labelText: "密码",
+                  hintText: "请输入密码",
+                  prefixIcon: Icon(Icons.lock)),
+              onSaved: (value) {},
+              obscureText: true,
+              validator: (value) {
+                return value.trim().length > 6 ? null : "密码不能少于六位";
+              },
+            ),
+            RaisedButton(
+              child: Text("提交"),
+              onPressed: () {
+                //此处不能使用form.of(context) context不是根目录的context 这里的context是FormBox
+                if ((_formkey.currentState as FormState).validate()) {
+                  (_formkey.currentState as FormState).save();
+                  // print('$userinfor.name,$userinfor.word,$userinfor.gender');
+                }
+                ;
+              },
+            ),
+            Builder(
+              builder: (ctx) {
+                return RaisedButton(
+                  child: Text("不同content的按钮"),
+                  onPressed: () {
+                    Form.of(ctx).validate();
+                  },
+                );
+              },
+            )
+          ],
+        ));
   }
 }
