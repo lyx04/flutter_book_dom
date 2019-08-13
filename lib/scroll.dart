63784c4bc3ae55cfa31e1fbe09847b03d8f0d284
lyx04 <https://github.com/lyx04>
2019-08-13 11:21:01 +0800

    提交

--- lib/scroll.dart
+++ lib/scroll.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:english_words/english_words.dart";
 
 class ScrollBox extends StatefulWidget {
   @override
@@ -8,15 +9,21 @@ class ScrollBox extends StatefulWidget {
 class _ScrollBoxState extends State<ScrollBox> {
   @override
   Widget build(BuildContext context) {
-    return Scrollbar(
-        child: SingleChildScrollView(
-            // scrollDirection: Axis.,
-            // physics:BouncingScrollPhysics() ,
-            physics: ClampingScrollPhysics(), //滑动到边界的样式
-            child: Center(
-                child: Column(
-              children: <Widget>[SingleBox(),ListViewBox(),ListBuildBox(),ListSeparatedBox(),ListSeparatedBox()],
-            ))));
+    return PositionTitle();
+    // Scrollbar(
+    //     child: SingleChildScrollView(
+    //         // scrollDirection: Axis.,
+    //         // physics:BouncingScrollPhysics() ,
+    //         physics: ClampingScrollPhysics(), //滑动到边界的样式
+    //         child: Center(
+    //             child: Column(
+    //           children: <Widget>[
+    //  InfiniteList()
+    //             // SingleBox(),
+    //             // ListViewBox(),
+    //             // ListBuildBox(),
+    //             ],
+    //         ))));
   }
 }
 
@@ -52,7 +59,7 @@ class _ListViewBoxState extends State<ListViewBox> {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      itemExtent: 100.0,
+      // itemExtent: 100.0,
       shrinkWrap: true,
       children: <Widget>[
         const Text('I\'m dedicating every day to you'),
@@ -66,6 +73,7 @@ class _ListViewBoxState extends State<ListViewBox> {
     );
   }
 }
+
 class ListBuildBox extends StatefulWidget {
   @override
   _ListBuildBoxState createState() => _ListBuildBoxState();
@@ -75,10 +83,12 @@ class _ListBuildBoxState extends State<ListBuildBox> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      shrinkWrap:true,
+      shrinkWrap: true,
       itemCount: 100,
-      itemBuilder: (BuildContext context,int itemnum){
-        return ListTile(title: Text("$itemnum"),);
+      itemBuilder: (BuildContext context, int itemnum) {
+        return ListTile(
+          title: Text("$itemnum"),
+        );
       },
     );
   }
@@ -92,17 +102,111 @@ class ListSeparatedBox extends StatefulWidget {
 class _ListSeparatedBoxState extends State<ListSeparatedBox> {
   @override
   Widget build(BuildContext context) {
-    Widget divider1 = Divider(color:Colors.blue);//下划线组件
-    Widget divider2 = Divider(color:Colors.green);
+    Widget divider1 = Divider(color: Colors.blue); //下划线组件
+    Widget divider2 = Divider(color: Colors.green);
     return ListView.separated(
-      shrinkWrap:true,
+      shrinkWrap: true,
       itemCount: 100,
-      separatorBuilder: (BuildContext context,int index){
-        return index%2==0?divider1:divider2;
+      separatorBuilder: (BuildContext context, int index) {
+        return index % 2 == 0 ? divider1 : divider2;
       },
-      itemBuilder: (BuildContext context,int itemnum){
-        return ListTile(title: Text("$itemnum"),);
+      itemBuilder: (BuildContext context, int itemnum) {
+        return ListTile(
+          title: Text("$itemnum"),
+        );
       },
+    );
+  }
+}
+
+class InfiniteList extends StatefulWidget {
+  @override
+  _InfiniteListState createState() => _InfiniteListState();
+}
+
+class _InfiniteListState extends State<InfiniteList> {
+  static const loadingTag = "##loading##"; //表尾标记
+  var itemlist = <String>[loadingTag];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _retrieveDate();
+  }
+
+  void _retrieveDate() {
+    Future.delayed(Duration(seconds: 1)).then((res) {
+      itemlist.insertAll(itemlist.length - 1,
+          generateWordPairs().take(20).map((e) => e.asPascalCase).toList());
+      setState(() {
+        itemlist = itemlist;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      shrinkWrap: true,
+      separatorBuilder: (BuildContext context, int itemnum) {
+        return Divider(height: .0);
+      },
+      itemCount: itemlist.length,
+      itemBuilder: (BuildContext context, int itemnum) {
+        if (itemlist[itemnum] == loadingTag) {
+          if (itemlist.length < 100) {
+            _retrieveDate();
+            return Container(
+                padding: const EdgeInsets.all(16.0),
+                alignment: Alignment.center,
+                child: CircularProgressIndicator());
+          } else {
+            return Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(16.0),
+                child: Text("没有更多了", style: TextStyle(color: Colors.grey)));
+          }
+        }
+        return ListTile(title: Text(itemlist[itemnum]));
+      },
+    );
+  }
+}
+
+class PositionTitle extends StatefulWidget {
+  @override
+  _PositionTitleState createState() => _PositionTitleState();
+}
+
+class _PositionTitleState extends State<PositionTitle> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ListTile(title: Text("商品列表")),
+        Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(0),
+          itemCount: 100,
+          itemBuilder: (BuildContext context, int itemnum) {
+            return ListTile(title: Text("$itemnum"));
+          },
+        )),
+      ],
+    );
+  }
+}
+
+class GridBox extends StatefulWidget {
+  @override
+  _GridBoxState createState() => _GridBoxState();
+}
+
+class _GridBoxState extends State<GridBox> {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      
     );
   }
 }
