4b0e3fca12fdc8eb01b5a678d57b40148ee2f63d
lyx04 <584719733@qq.com>
2019-08-26 07:39:01 +0800

    7.2

--- lib/Feature.dart
+++ lib/Feature.dart
@@ -8,6 +8,7 @@ class FeatureBox extends StatelessWidget {
       shrinkWrap: true,
       children: <Widget>[
         WillPopBox(),
+        InheritedWidgetTestRoute()
       ],
     ));
   }
@@ -52,3 +53,64 @@ class _WillPopBoxState extends State<WillPopBox> {
   }
 }
 
+
+
+class ShareDataWidget extends InheritedWidget {
+  ShareDataWidget({@required this.data,Widget child}):super(child:child);
+  final int data;
+  static ShareDataWidget of(BuildContext context){
+      return context.inheritFromWidgetOfExactType(ShareDataWidget);
+  }
+
+  @override
+  bool updateShouldNotify(ShareDataWidget old){
+    return old.data!=data;
+  }
+}
+
+class TestWidget extends StatefulWidget {
+  @override
+  _TestWidgetState createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(ShareDataWidget.of(context).data.toString());
+  }
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    //父或祖先widget中的InheritedWidget改变(updateShouldNotify返回true)时会被调用。
+    //如果build中没有依赖InheritedWidget，则此回调不会被调用。
+    print("Dependencies change");
+  }
+}
+
+class InheritedWidgetTestRoute extends StatefulWidget {
+  @override
+  _InheritedWidgetTestRouteState createState() => _InheritedWidgetTestRouteState();
+}
+
+class _InheritedWidgetTestRouteState extends State<InheritedWidgetTestRoute> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return ShareDataWidget(
+      data: count,
+      child: Column(
+        children: <Widget>[
+          TestWidget(),
+          RaisedButton(
+            child: Text("添加一个"),
+            onPressed: (){
+              setState(() {
+               count++; 
+              });
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
