0bd6f12a7c3d92258873177ed49a0d4d0887305f
lyx04 <584719733@qq.com>
2019-09-17 06:17:18 +0800

    8.1

--- lib/event.dart
+++ lib/event.dart
@@ -5,7 +5,7 @@ class EventBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scrollbar(
       child: Column(
-        children: <Widget>[ListenerBox()],
+        children: <Widget>[ListenerBox(),BehaviorTranslucentBox()],
       ),
     );
   }
@@ -28,5 +28,36 @@ class _ListenerBoxState extends State<ListenerBox> {
         child: ConstrainedBox(
             constraints: BoxConstraints.tight(Size(200.0, 150.0)),
             child: Center(child: Text("Box a"))));
+  }
+}
+
+class BehaviorTranslucentBox extends StatefulWidget {
+  @override
+  _BehaviorTranslucentBoxState createState() => _BehaviorTranslucentBoxState();
+}
+
+class _BehaviorTranslucentBoxState extends State<BehaviorTranslucentBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+  children: <Widget>[
+    Listener(
+      child: ConstrainedBox(
+        constraints: BoxConstraints.tight(Size(300.0, 200.0)),
+        child: DecoratedBox(
+            decoration: BoxDecoration(color: Colors.blue)),
+      ),
+      onPointerDown: (event) => print("down0"),
+    ),
+    Listener(
+      child: ConstrainedBox(
+        constraints: BoxConstraints.tight(Size(200.0, 100.0)),
+        child: Center(child: Text("左上角200*100范围内非文本区域点击")),
+      ),
+      onPointerDown: (event) => print("down1"),
+      //behavior: HitTestBehavior.translucent, //放开此行注释后可以"点透"
+    )
+  ],
+);
   }
 }
