79964df5b8dcf1cb4b880164809b4cfafe8a057e
lyx04 <584719733@qq.com>
2019-11-07 06:08:33 +0800

    11.2

--- lib/IO/httpclient.dart
+++ lib/IO/httpclient.dart
@@ -14,11 +14,17 @@ class _HttpclientRouteState extends State<HttpclientRoute> {
   HttpClient _httpClient = new HttpClient();
   @override
   Widget build(BuildContext context) {
+    _httpClient.userAgent =
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1";
     return ConstrainedBox(
         constraints: BoxConstraints.expand(),
         child: Column(
           children: <Widget>[
-            // Image.network("https://pics7.baidu.com/feed/43a7d933c895d143d999588f497236075baf0732.jpeg?token=4c08e6b9f588212ab501a2b07996bab4&s=B19A71943401DAEF8AB7FEC10300F0A9",width: 100.0,height: 100.0,),
+            Image.network(
+              "https://pics7.baidu.com/feed/43a7d933c895d143d999588f497236075baf0732.jpeg?token=4c08e6b9f588212ab501a2b07996bab4&s=B19A71943401DAEF8AB7FEC10300F0A9",
+              width: 100.0,
+              height: 100.0,
+            ),
             RaisedButton(
               child: Text("获取百度"),
               onPressed: loading
@@ -32,8 +38,7 @@ class _HttpclientRouteState extends State<HttpclientRoute> {
                         // _httpClient.connectionTimeout = Duration( minutes: 2);
                         HttpClientRequest request = await _httpClient
                             .getUrl(Uri.parse("https://www.baidu.com"));
-                            // .getUrl(Uri.parse("https://news-at.zhihu.com/api/4/news/latest"));
-                        request.headers.add("user-agent","Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1");
+                        // .getUrl(Uri.parse("https://news-at.zhihu.com/api/4/news/latest"));
                         HttpClientResponse response = await request.close();
                         txt = await response.transform(utf8.decoder).join();
                         print(response.headers);
@@ -48,15 +53,43 @@ class _HttpclientRouteState extends State<HttpclientRoute> {
                       }
                     },
             ),
+            RaisedButton(
+              child: Text("svn尝试"),
+              onPressed: loading
+                  ? null
+                  : () async {
+                      try {
+                        _httpClient.addCredentials(
+                          Uri.parse(
+                              "https://svn.hseduyun.net/svn/message_center/message_center/trunk"),
+                          "admin",
+                          HttpClientBasicCredentials(
+                              "liangyuxuan", "liangyuxuan0320"),
+                        );
+                        HttpClientRequest request = await _httpClient.getUrl(
+                          Uri.parse(
+                              "https://svn.hseduyun.net/svn/message_center/message_center/trunk"),
+                        );
+                        HttpClientResponse response = await request.close();
+                        print(await response.transform(utf8.decoder).join());
+                        request.close();
+                      } catch (e) {
+                        print("请求失败$e");
+                      } finally {
+                        setState(() {
+                          loading = false;
+                        });
+                      }
+                    },
+            ),
             Expanded(
-              child:Scrollbar(
+                child: Scrollbar(
               child: SingleChildScrollView(
                 child: Container(
                   child: Text(txt),
                 ),
               ),
-            )
-            )
+            ))
           ],
         ));
   }
