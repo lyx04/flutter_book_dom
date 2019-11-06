375cfdf59dfa812e2c3e0a407ba2f81787589ce0
梁宇轩 <https://github.com/lyx04>
2019-11-06 17:31:52 +0800

    11.2

---
+++ lib/IO/diohttpclient.dart
@@ -0,0 +1,4 @@
+import "package:flutter/material.dart";
+import "package:dio/dio.dart";
+
+Dio dio = Dio();
--- lib/IO/httpclient.dart
+++ lib/IO/httpclient.dart
@@ -18,6 +18,7 @@ class _HttpclientRouteState extends State<HttpclientRoute> {
         constraints: BoxConstraints.expand(),
         child: Column(
           children: <Widget>[
+            // Image.network("https://pics7.baidu.com/feed/43a7d933c895d143d999588f497236075baf0732.jpeg?token=4c08e6b9f588212ab501a2b07996bab4&s=B19A71943401DAEF8AB7FEC10300F0A9",width: 100.0,height: 100.0,),
             RaisedButton(
               child: Text("获取百度"),
               onPressed: loading
@@ -28,14 +29,17 @@ class _HttpclientRouteState extends State<HttpclientRoute> {
                         txt = "正在请求";
                       });
                       try {
+                        // _httpClient.connectionTimeout = Duration( minutes: 2);
                         HttpClientRequest request = await _httpClient
                             .getUrl(Uri.parse("https://www.baidu.com"));
-                        // request.headers.add("user-agent","Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1");
+                            // .getUrl(Uri.parse("https://news-at.zhihu.com/api/4/news/latest"));
+                        request.headers.add("user-agent","Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1");
                         HttpClientResponse response = await request.close();
                         txt = await response.transform(utf8.decoder).join();
                         print(response.headers);
                         request.close();
                       } catch (e) {
+                        print(e);
                         txt = "请求失败：$e";
                       } finally {
                         setState(() {
