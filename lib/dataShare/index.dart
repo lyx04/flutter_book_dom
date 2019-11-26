9104d96a159a61d9bac6181d0e92cb7f4334040c
梁宇轩 <https://github.com/lyx04>
2019-11-26 17:26:12 +0800

    InheritedWidget

--- lib/dataShare/index.dart
+++ lib/dataShare/index.dart
@@ -16,10 +16,10 @@ class DataShare extends InheritedWidget {
   //该回调决定当data发生变化时，是否通知子树中依赖data的Widget
   @override
   bool updateShouldNotify(DataShare oldWidget) {
-    print("-------------------------------");
-    print(oldWidget.data);
-    print(data);
-    print("-------------------------------");
+    // print("-------------------------------");
+    // print(oldWidget.data);
+    // print(data);
+    // print("-------------------------------");
     //如果返回true，则子树中依赖（build函数中有调用）本Widget的子Widget的State.didChangeDependencies会被调用
     return oldWidget.data != data;
   }
@@ -60,6 +60,143 @@ class _Test1WidgetState extends State<Test1Widget> {
             child: Text("自增1"),
           )
         ],
+      ),
+    );
+  }
+}
+
+class ShopInheritedWidget extends InheritedWidget {
+  ShopInheritedWidget({Key key, this.list, Widget child}) : super(child: child);
+  final List list;
+  @override
+  bool updateShouldNotify(ShopInheritedWidget oldWidget) {
+    print(list);
+    return true;
+  }
+
+  static ShopInheritedWidget of(BuildContext context) {
+    return context.inheritFromWidgetOfExactType(ShopInheritedWidget);
+  }
+}
+
+class ShopContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (BuildContext context) {
+                return ShopList();
+              }),
+            );
+          },
+          child: Text("开始"),
+        ),
+      ),
+    );
+  }
+}
+
+class ShopList extends StatefulWidget {
+  @override
+  _ShopListState createState() => _ShopListState();
+}
+
+class _ShopListState extends State<ShopList> {
+  List shoplist = [];
+  List shopList = [
+    {"name": "card", "money": "10"},
+    {"name": "fsdf", "money": "30"},
+    {"name": "gsdf", "money": "20"},
+    {"name": "cvxb", "money": "40"},
+    {"name": "erw", "money": "50"},
+    {"name": "asdf", "money": "60"},
+    {"name": "zcxxv", "money": "70"},
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("购物车"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shop),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return ShopCard();
+                  },
+                ),
+              );
+            },
+          )
+        ],
+      ),
+      body: ShopInheritedWidget(
+        list: shoplist,
+        child: ListView(
+          children: shopList.map((e) {
+            var name = e['name'];
+            return ListTile(
+              title: Text("$name"),
+              onTap: () {
+                shoplist.add(e);
+              },
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class ShopCard extends StatefulWidget {
+  @override
+  _ShopCardState createState() => _ShopCardState();
+}
+
+class _ShopCardState extends State<ShopCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("购物车详情"),
+      ),
+      body: ShopInheritedWidget(
+        child: Column(
+          children: <Widget>[
+            Builder(
+              builder: (BuildContext context1) {
+                print(context1);
+                print("--------------------------------------------");
+                print(ShopInheritedWidget.of(context1).list);
+                return Text("fsd");
+                // return Expanded(
+                //   child: ListView(
+                //     children: ShopInheritedWidget.of(context1).list.map((e) {
+                //       var name = e['name'];
+                //       return ListTile(
+                //         title: Text("$name"),
+                //       );
+                //     }).toList(),
+                //   ),
+                // );
+              },
+            ),
+            // Builder(
+            //   builder: (BuildContext context) {
+            //     var money = 0;
+            //     ShopInheritedWidget.of(context).list.forEach((e) {
+            //       money += int.parse(e["money"]);
+            //     });
+            //     return Expanded(child: Text("$money"));
+            //   },
+            // )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body: Test1Widget()));
+    return MaterialApp(home: Scaffold(body: ShopContainer()));
   }
 }
 
